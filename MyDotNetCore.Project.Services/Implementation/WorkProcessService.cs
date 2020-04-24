3478dc5fd6f3e262dbb557a100104f52e4662b6f
ahuinan <ahuinan@qq.com>
2020-04-24 14:27:55 +0800

    增加WorkProcess

---
+++ MyDotNetCore.Project.Services/Implementation/WorkProcessService.cs
@@ -0,0 +1,15 @@
+﻿using MyCode.Project.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDotNetCore.Project.Services.Implementation
+{
+    /// <summary>
+    /// 基于数据库的调度表
+    /// </summary>
+    public class WorkProcessService: ServiceBase,IWorkProcessService
+    {
+        private readonly ISysWorkprocessRepository _sysWorkprocessRepository;
+    }
+}
---
+++ MyDotNetCore.Project.Services/ServiceBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDotNetCore.Project.Services
+{
+    /// <summary>
+    /// 服务基类
+    /// </summary>
+    public abstract class ServiceBase
+    {
+    }
+}
